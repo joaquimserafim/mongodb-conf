9ca77bc48aeb2e732968be5bb7c8e9ce88a23e7e
joaquimserafim <joaquim.serafim@gmail.com>
2014-11-21 00:52:21 +0000

    json 2 yaml done

--- index.js
+++ index.js
@@ -2,33 +2,35 @@
 
 var fs    = require('fs');
 var path  = require('path');
-var split = require('split');
 var yaml  = require('yamljs');
 var ut    = require('./ut');
 
 module.exports = parser;
 
 function parser() {
-  function readConfFile(ext, filePath, cb) {
-    if (ext !== path.extname(filePath)) {
-      cb(new Error('mongod.conf with wrong extension!'));
-    } else {
-      var data = [];
-      fs.createReadStream(path.join(__dirname, filePath))
-        .pipe(split())
-        .on('data', function(line) {
-          if (0 !== line.indexOf('#') && line) {
-            data.push(line);
+  function readFile(isJson, file, cb) {
+    fs.readFile(file, 'utf8', function(err, data) {
+      if (err) {
+        cb(err);
+      } else {
+        if (isJson) {
+          var json = ut.parseJSON(data);
+          if (json) {
+            cb(null, json);
+          } else {
+            cb(new Error('invalid JSON file!'));
           }
-        })
-        .on('error', cb)
-        .on('end', function() {
-          cb(null, data);
-        });
-    }
+        } else {
+          var lines = data.split(/\r?\n/).filter(function(line) {
+            return !!line.indexOf('#') && line;
+          });
+          cb(null, lines);
+        }
+      }
+    });
   }
 
-  function confToJson(path, cb) {
+  function conf2json(path, cb) {
     function readFileConfCb(err, data) {
       if (err) {
         cb(err);
@@ -42,25 +44,24 @@ function parser() {
       }
     }
 
-    readConfFile('.conf', path, readFileConfCb);
+    readFile(false, path, readFileConfCb);
   }
 
-  function jsonToYaml(path, cb) {
+  function json2yaml(path, cb) {
     function readFileJSONCb(err, data) {
       if (err) {
         cb(err);
       } else {
-        var json = ut.parseJSON(data);
-        var yamlString = yaml.stringify(json, 4);
+        var yamlString = yaml.stringify(data, 4);
         cb(null, yamlString);
       }
     }
 
-    readConfFile('.json', path, readFileJSONCb);
+    readFile(true, path, readFileJSONCb);
   }
 
   return {
-    toJSON: confToJson,
-    jsonToYaml: jsonToYaml
+    conf2json: conf2json,
+    json2yaml: json2yaml
   };
 }
--- test/index.js
+++ test/index.js
@@ -3,10 +3,11 @@
 var tape    = require('tape');
 var parser  = require('../index')();
 var ut      = require('../ut');
+var yaml    = require('yamljs');
 
 
 tape('convert a `conf` file to JSON', function(assert) {
-  function parseConfToJson(err, json) {
+  function conf2jsonCb(err, json) {
     if (err) {
       assert.fail(err);
     } else {
@@ -14,30 +15,38 @@ tape('convert a `conf` file to JSON', function(assert) {
       assert.end();
     }
   }
-  parser.toJSON('test/fixtures/mongod.conf', parseConfToJson);
+  parser.conf2json('test/fixtures/mongod.conf', conf2jsonCb);
 });
 
-tape('try to convert a bad `conf` file', function(assert) {
-  function parseConfToJson(err, json) {
+tape('a bad `conf` file', function(assert) {
+  function conf2jsonCb(err) {
     if (err) {
-      assert.equal(err.message, 'mongod.conf with wrong extension!');
+      assert.equal(err.message, 'ENOENT, open \'test/fixtures/mongod.con\'');
       assert.end();
-    } else {
-      // never should enter in here
-      assert.fail(json);
     }
   }
-  parser.toJSON('test/fixtures/mongod.cof', parseConfToJson);
+  parser.conf2json('test/fixtures/mongod.con', conf2jsonCb);
 });
 
 tape('convert from JSON to YAML', function(assert) {
-  function parseJsonToYaml(err, yaml) {
+  function json2yamlCb(err, yamlString) {
     if (err) {
       assert.fail(err);
     } else {
-      assert.pass(yaml);
+      assert.deepEqual(typeof yamlString, 'string');
+      assert.deepEqual(ut.isJSObject(yaml.parse(yamlString)), true);
       assert.end();
     }
   }
-  parser.jsonToYaml('test/fixtures/mongodb_conf.json', parseJsonToYaml);
+  parser.json2yaml('test/fixtures/mongodb_conf.json', json2yamlCb);
+});
+
+tape('a bad `JSON` file', function(assert) {
+  function json2yamlCb(err) {
+    if (err) {
+      assert.equal(err.message, 'invalid JSON file!');
+      assert.end();
+    }
+  }
+  parser.json2yaml('test/fixtures/bad_json_file.json', json2yamlCb);
 });
